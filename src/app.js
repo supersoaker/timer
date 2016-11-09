492f315d2cbe941255d4a64a0df9e6180ce98b7f
marlon.ruescher <m.ruescher@gds-gmbh.de>
2016-11-09 17:01:15 +0100

    refactored js structure and added timer init funtions

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@
 import os from 'os'; // native node.js module
 import { remote } from 'electron'; // native electron module
 import jetpack from 'fs-jetpack'; // module loaded from npm
-import { greet } from './hello_world/hello_world'; // code authored by you in this project
+import timer from './timer/init';
 import env from './env';
 
 console.log('Loaded environment variables:', env);
@@ -17,8 +17,4 @@ var appDir = jetpack.cwd(app.getAppPath());
 // here files like it is node.js! Welcome to Electron world :)
 console.log('The author of this app is:', appDir.read('package.json', 'json').author);
 
-document.addEventListener('DOMContentLoaded', function () {
-    document.getElementById('greet').innerHTML = greet();
-    document.getElementById('platform-info').innerHTML = os.platform();
-    document.getElementById('env-name').innerHTML = env.name;
-});
+timer();
---
+++ src/timer/init.js
@@ -0,0 +1,48 @@
+export default function init() {
+    jQuery(document).ready(function($) {
+
+        (function initializeTimerOptions() {
+            var html = '';
+            var i = 0;
+            // Iterate the options for minutes and second from 0 to 59
+            for(i; i<60; i++)
+                html += '<option value="'+i+'">'+i+'</option>';
+
+            $('#minute-selector, #second-selector').html(html);
+
+            // Iterate the options for hours from 0 to 23
+            for(i = 0, html = ''; i<25; i++)
+                html += '<option value="'+i+'">'+i+'</option>';
+
+            $('#hour-selector').html(html);
+
+            // Initialize material design
+            $('select').material_select();
+        }());
+
+        (function addTextAreaTabFix() {
+            $(document).delegate('textarea', 'keydown', function(e) {
+                var keyCode = e.keyCode || e.which;
+
+                // If tab key was clicked
+                if (keyCode == 9) {
+                    e.preventDefault();
+                    var start = $(this).get(0).selectionStart;
+                    var end = $(this).get(0).selectionEnd;
+
+                    // set textarea value to: text before caret + tab + text after caret
+                    $(this).val($(this).val().substring(0, start)
+                        + "\t"
+                        + $(this).val().substring(end));
+
+                    // put caret at right position again
+                    $(this).get(0).selectionStart =
+                        $(this).get(0).selectionEnd = start + 1;
+                }
+            });
+        }());
+
+    });
+}
+
+
